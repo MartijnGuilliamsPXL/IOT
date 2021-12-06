910ad20b86794f81cf48cae96e5bf62a422a2b01
MartijnGuilliamsPXL <56915229+MartijnGuilliamsPXL@users.noreply.github.com>
2021-12-06 10:50:13 +0100

    opruimen bestanden

---
+++ database.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost";
+$username = "student_11901231";
+$password = "csOgBOHWisQx";
+$dbname = "student_11901231";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+?>
