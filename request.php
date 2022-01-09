c6cf3ca0ebbc4c8737b2049e50d4b1a26b728bf1
MartijnGuilliamsPXL <56915229+MartijnGuilliamsPXL@users.noreply.github.com>
2022-01-09 14:35:41 +0100

    update

--- request.php
+++ request.php
@@ -10,7 +10,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "INSERT INTO data_iot (id, waarde, tijd) VALUES ($id, $waarde, now())";
+$sql = "INSERT INTO Sensor_waardes (sensor_id, waarde, datum) VALUES ($id, $waarde, now())";
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully!" . "<br>" . "id is $id" . "<br>" . "waarde is $waarde";
 }
